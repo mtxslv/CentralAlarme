d043c95eafe9042122fbb86dab0912d4d974d634
Mateus de Assis <mateus.d.assis.silva@gmail.com>
2021-04-17 14:59:47 -0300

    shiftInOut completo com projeto do proteus adicionado

--- shiftInOut/shiftInOut/main.c
+++ shiftInOut/shiftInOut/main.c
@@ -43,12 +43,13 @@ ler periodicamente o registrador.
 #define clock_shftin PINB2
 #define P_Shftin PINB1
 // talvez possa ser o mesmo clock do extensor entrada
-#define clock_shftout PINB3 
-#define Pinout_serial PINB4
+#define clock_shftout PINB3 // o clock PINB2 deve ser usado, não esse.
+#define Pinout_serial PINC0 // saída de dados serial
+#define pLoad_pin PINB7 // controle carga paralela
 
 
 uint8_t varLeituraSerial = 0;
-uint8_t varEscritaSerial = 0xAA;
+uint8_t varEscritaSerial = 0xFF;
 
 
 uint8_t SetBit(uint8_t valor, uint8_t posicao)
@@ -64,7 +65,7 @@ uint8_t ClearBit(uint8_t valor, uint8_t posicao){
 
 void OutSerial()
 {
-    PINB |= (1 << Pinout_serial);
+    PINC |= (1 << Pinout_serial);
 }
 
 void shiftIn()
@@ -92,13 +93,39 @@ void shiftIn()
 	}
 }
 
+/*
+Como ShiftOut funciona?
+
+1. ST_CP <- LOW
+2. loop 8x
+3.		dataPin <- bit a ser enviado
+4.		SH_CP <- 1
+5.		SH_CP <- 0 
+6. ST_CP <- HIGH 
+
+DS == serial data transfer pin
+ST_CP == parallel load pin
+SH_CP == clock pin
+*/
+
 void shiftOut() // ainda não testei mas fiz a parte de Setar e dar Clear no pino
 {
     // pinos de controle do CI
-    
-    
-   for (uint8_t iter = 0; iter<8 ; iter++)
-   {
+//    PINB &= ~(1<<pLoad_pin);// PINB7
+		for (uint8_t iter = 0; iter<8; iter++){
+			if( ((varEscritaSerial>>iter)& 0x1) ) // aqui deve vir a resposta a "qual o bit que está guardado na posição iter do byte a ser enviado?"
+			PORTC |= 0x01; // 0b00000001 -> ligar PORTC0. (Pinout_serial)
+			else
+			PORTC &= 0xFE;// 0b11111110 -> desligar PORTC0. (Pinout_serial)
+			
+			PORTB |= (1 << clock_shftin); // PINB3,CLK vai pra 1
+			PORTB &= ~(1 << clock_shftin); // PINB3,CLK vai pra 0
+		}
+		PINB &= ~(1<<pLoad_pin);
+		PINB |= (1<<pLoad_pin); // PINB7
+/*	    
+   for (uint8_t iter = 0; iter<8 ; iter++){
+
        if(PINB & (1 << Pinout_serial))
        {
            if(varEscritaSerial & ~(1<<iter)) OutSerial(); // QUANDO FOR 0, COLOCA A SAÍDA PRA 1
@@ -106,25 +133,36 @@ void shiftOut() // ainda não testei mas fiz a parte de Setar e dar Clear no pino
        else
        {
            if(varEscritaSerial & (1<<iter)) OutSerial(); // QUANDO FOR 1, IRÁ SETAR O VALOR NO REGISTRADOR
-       }
-       
-       PORTB |= (1 << clock_shftout); // CLK vai pra 1
-       PORTB &= ~(1 << clock_shftout); // CLK vai pra 0
+       }           
+
+	   
+	   
+	   PORTB |= (1 << clock_shftout); // PINB3,CLK vai pra 1 
+       PORTB &= ~(1 << clock_shftout); // PINB3,CLK vai pra 0 
 	}
-    
+	// parallel load
+	PINB |= (1<<pLoad_pin); // PINB7
+    PINB &= ~(1<<pLoad_pin);
+*/	
 }
 
 int main(void)
 {
 	DDRB &= 0b11111110; // o pino PB0 ser? usado como leitura do serial, 
-	DDRB |= 0b00000110; // os pinos PB1 e PB2 ser? a sa?da de clock (CLK) e o controle paralelo/serial (P/S')
+	DDRB |= 0b10001110; // os pinos PB1 e PB2 ser? a sa?da de clock (CLK) e o controle paralelo/serial (P/S')
 	DDRD = 0xFF; // PORTD ? sa?da (s? pra teste)
     /* Replace with your application code */
+	DDRC |= 0b00000001; // saída dados serial
     while (1) 
-    {
-        //shiftOut();
+    { 
+		
+	
+		
+        //
 		shiftIn();
-		PORTD = varLeituraSerial;
+		varEscritaSerial = varLeituraSerial;
+		shiftOut();
+		//PORTD = varLeituraSerial;
 
         //varLeituraSerial = 0;
     }
